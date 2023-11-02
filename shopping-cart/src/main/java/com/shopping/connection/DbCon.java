2ae63345253cc7b72ebb80299f85e3cc31c732cc
Ayush Ghetia <124595637+AyushGhetia@users.noreply.github.com>
2023-11-03 00:06:53 +0530

    Add files via upload

---
+++ shopping-cart/src/main/java/com/shopping/connection/DbCon.java
@@ -0,0 +1,17 @@
+package com.shopping.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbCon {
+	private static Connection connection = null;
+	public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        if(connection == null){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ayush", "root", "Ayush@2003"); // Assign the connection to the 'connection' variable
+            System.out.print("connected");
+        }
+        return connection;
+    }
+}
